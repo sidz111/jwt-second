be586ccbd1d87c672a95b2683df44eb77a0fd065
sidz111 <sssurwade2212@gmail.com>
2026-03-09 16:26:23 +0530

    generated jwt token

--- models/student.go
+++ models/student.go
@@ -5,6 +5,7 @@ import "gorm.io/gorm"
 type Student struct {
 	gorm.Model
 	Name     string `json:"name"`
+	Username string `json:"username" gorm:"unique"` // this is for authentication and should be unique
 	Age      int    `json:"age"`
 	Class    string `json:"class"`
 	Token    string `json:"token"`
---
+++ utils/jwt.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var SECRET_KEY = []byte("supersecretkey")
+
+func GenerateJWT(username string, id uint) (string, error) {
+	claims := jwt.MapClaims{
+		"username": username,
+		"user_id":  id,
+		"exp":      time.Now().Add(time.Hour * 24).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(SECRET_KEY)
+	if err != nil {
+		return "no jwt", err
+	}
+	return tokenString, nil
+}
